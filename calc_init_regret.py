e38c1ea3c15cf687c1d7429be2fe28833a1ac4ad
Yifan Zhu <fanzhuyifan@gmail.com>
2022-06-27 10:42:50 -0700

    update initial regret

--- calc_init_regret.py
+++ calc_init_regret.py
@@ -4,11 +4,11 @@ from tqdm import tqdm
 import generate as generate
 
 
-def calc_init_regret(d, M, noise, N_test, N_trials):
+def calc_init_regret(d, M, noise, act, N_test, N_trials):
     result = np.zeros(N_trials)
     for i in range(N_trials):
         (thetan, an, bn) = generate.generate_single_layer(M, d, 1)
-        (_, Y_test) = generate.generate_single_data(N_test, an, bn, thetan)
+        (_, Y_test) = generate.generate_single_data(N_test, an, bn, thetan, act)
         result[i] = generate.kl_divergence(Y_test.reshape(-1), 0, noise)
     return result
 
@@ -16,10 +16,13 @@ def calc_init_regret(d, M, noise, N_test, N_trials):
 def main(filename, N_test, N_trials):
     data = pd.read_excel(filename)
     (N, n_cols) = data.shape
-    assert(n_cols == 5)
+    assert(n_cols == 6)
     for i in tqdm(range(N)):
         regrets = calc_init_regret(
-            data['d'][i], data['M'][i], data['noise'][i], N_test, N_trials)
+            data['d'][i], data['M'][i],
+            data['noise'][i], data['act'][i],
+            N_test, N_trials
+        )
         data.at[i, 'initial-regret'] = np.mean(regrets)
         # standard deviation of sample mean needs to be divided by sqrt(N_trials)
         data.at[i, 'std-dev'] = np.std(regrets) / np.sqrt(N_trials)
