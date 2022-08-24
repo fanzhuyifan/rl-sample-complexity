1c66741ff86c079ce6ecdf1bd0cebf5883631aca
Yifan Zhu <fanzhuyifan@gmail.com>
2022-08-23 21:24:48 -0700

    generate

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+# Overview of files
+## `generate.py`
+Helper functions related to generating single-hidden-layer teacher networks, generating data from these teacher networks, and calculating error.
+
+# Example Code
--- generate.py
+++ generate.py
@@ -1,4 +1,4 @@
-""" Generate random data-generating environments
+""" Generate random single-hidden-layer teacher networks.
 """
 
 import numpy as np
@@ -8,9 +8,9 @@ def generate_single_layer(
     M, d, num,
 ):
     """
-    :param M: the sparsity
-    :param d: the dimension of the input
-    :param num: number of gs to generate
+    :param M: width (number of hidden neurons)
+    :param d: input dimension
+    :param num: number of teacher networks to generate
     :return: (thetan, an, bn), where thetan has shape (num, M), an has shape (num, M, d), and bn has shape (num, M)
     """
     an = np.random.normal(0, 1 / np.sqrt(d + 1), size=(num, M, d))
@@ -20,13 +20,13 @@ def generate_single_layer(
 
 
 def generate_single_data(
-    T, an, bn, thetan, act="sign",
+    N, an, bn, thetan, act="sign",
 ):
-    """ Generate data of length T according to the single layer generation process g specified by the parameters
+    """ Generate noiseless data of length N according to the single-hidden-layer teacher network g specified by the parameters
     :param an: has shape (num, M, d)
     :param bn: has shape (num, M)
     :param thetan: shape (num, M)
-    :param T: number of data to generate for each g
+    :param N: number of data to generate for each g
     :param act: the activation function
     :return: (X, Y), where X has shape (num, T, d), and Y has shape (num, T).
     X has standard Gaussian distribution, and the corresponding Y=g(X)
@@ -37,13 +37,12 @@ def generate_single_data(
     }
     act = acts[act]
     (num, M, d) = an.shape
-    # X: (num, T, d)
-    X = np.random.normal(0, 1, (num, T, d))
+    X = np.random.normal(0, 1, (num, N, d))
     Y = (an.reshape(num, 1, M, 1, d) @
-         X.reshape(num, T, 1, d, 1)).reshape(num, T, M)
+         X.reshape(num, N, 1, d, 1)).reshape(num, N, M)
     Y += bn.reshape(num, 1, M)
     Y = act(Y)
-    Y = (thetan.reshape(num, 1, 1, M) @ Y.reshape(num, T, M, 1)).reshape(num, T)
+    Y = (thetan.reshape(num, 1, 1, M) @ Y.reshape(num, N, M, 1)).reshape(num, N)
     return (X, Y)
 
 
@@ -55,4 +54,7 @@ def add_noise(data, noise=0.1):
 
 
 def kl_divergence(y, y_hat, sigma):
+    """ Returns KL divergence of two normal distributions with standard deviation sigma.
+    This is equal to L2 loss inversely scaled by noise.
+    """
     return np.mean((y - y_hat) ** 2, axis=-1) / (2 * sigma ** 2)
